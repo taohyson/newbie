04a493f7b13c69ca3a48c411421bb0b9e9653550
zhujw <zhujw@mapbar.com>
2018-10-30 17:10:40 +0800

    newbie paths runoob

---
+++ 环境构件.bat
@@ -0,0 +1,13 @@
+@echo off
+
+chrome https://dev.mysql.com/
+chrome https://dev.mongodb.com/
+chrome https://github.com/MicrosoftArchive/redis/releases
+chrome https://www.sqlite.org/
+chrome https:www.mingw.org
+chrome http://www.oracle.com/technetwork/java/javase/downloads/index.html
+chrome http://maven.apache.org/download.cgi
+chrome https://www.python.org/
+chrome https://golang.org/dl/
+chrome https://github.com/rjpcomputing/luaforwindows/releases
+wget https://vuejs.org/js/vue.min.js
